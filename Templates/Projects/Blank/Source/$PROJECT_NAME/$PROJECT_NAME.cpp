50a8c19090e6ab8b5efe4bbf8b8f2fa237b7613a
MrDrElliot <133075253+MrDrElliot@users.noreply.github.com>
2026-01-25 13:35:21 -0600

    Deleted templates for refactor

--- Templates/Projects/Blank/Source/$PROJECT_NAME/$PROJECT_NAME.cpp
+++
@@ -1,5 +0,0 @@
-#include "$PROJECT_NAME.h"
-#include "Core/Module/ModuleManager.h"
-
-// Boilerplate module discovery and implementation. DO NOT REMOVE
-IMPLEMENT_MODULE(F$PROJECT_NAME, "$PROJECT_NAME")
