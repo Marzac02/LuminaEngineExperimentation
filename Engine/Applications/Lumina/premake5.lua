3f8810bd59c5cc4402d3f8554291f66b1edced6e
MrDrElliot <133075253+MrDrElliot@users.noreply.github.com>
2026-01-25 10:01:01 -0600

    Possibly done

--- Engine/Applications/Lumina/premake5.lua
+++ Engine/Applications/Lumina/premake5.lua
@@ -1,31 +1,19 @@
 project "Lumina"
 	kind "WindowedApp"
 	rtti "off"
-	dependson { "Lumina", "Editor", "ImGui", "EA", "Tracy", "lua54" }
 
 	defines
 	{ 
-		"_SILENCE_ALL_MS_EXT_DEPRECATION_WARNINGS",
 		"TRACY_ENABLE",
     	"TRACY_CALLSTACK",
     	"TRACY_ON_DEMAND",
 		"TRACY_IMPORTS",
 	}
 
-	libdirs
-	{
-		LuminaConfig.EnginePath("Engine/Source/ThirdParty/lua"),
-	}
-
 	links
-	 {
+	{
 		"Runtime",
 		"Editor",
-		"ImGui",
-    	"EA",
-    	"EnkiTS",
-		"Tracy",
-		"lua54",
 	}
 	 
 	files
